6ace4e988da891ee01d5ff93477c412a95eca1e5
Adam <alounsbu@alumni.uwo.ca>
2017-01-05 17:57:26 -0500

    Added encryption capability

--- affine.py
+++ affine.py
@@ -12,15 +12,16 @@ def affine_cipher(txt):
         while True:
             option = raw_input("Encrypt/Decrypt (e/d)?: ")
             if option == 'd' or option == 'e':
-                return option
+                key_get(option)
+                break
             else:
                 print 'Please choose a valid option for decryption/encryption.\n'
 
-    def key_get():
+    def key_get(option):
         while True:
-            user_key = raw_input("Enter an encryption/decryption key: ")
+            user_key = input("Enter an encryption/decryption key: ")
             if int(user_key):
-                operation(encrypt_decrypt(), user_key)
+                operation(option, user_key)
                 break
             else:
                 print "Please enter an integer value for the key.\n"
@@ -30,7 +31,7 @@ def affine_cipher(txt):
         check_keys(key_a, key_b, enc_dec)
 
         if ('e' or 'E') in enc_dec:
-            return encrypt()
+            return encrypt(key_a, key_b, txt)
         else:
             return decrypt()
 
@@ -42,23 +43,36 @@ def affine_cipher(txt):
     def check_keys(key_a, key_b, mode):
         if key_a == 1 and mode == 'e':
             print 'The affine cipher is very weak when key A computes to 1. Choose a different key.'
-            key_get()
+            key_get(mode)
         if key_b == 0 and mode == 'e':
             print 'The affine cipher is very weak when key B computes to 0. Choose a different key.'
-            key_get()
+            key_get(mode)
         if key_a < 0 or (len(char_set) - 1 < key_b < 0):
             print 'Key A must be greater than 0 and Key B must be between 0 and {0}'.format(len(char_set) - 1)
-            key_get()
+            key_get(mode)
         if gcd(key_a, len(char_set)) != 1:
             print 'Key A ({0}) and the symbol set size ({1}) are not relatively prime. Choose a different key.'.format(
                 key_a, len(char_set))
-            key_get()
+            key_get(mode)
+
+    def encrypt(key_a, key_b, message):
+        cipher_text = ''
+        for char in message:
+            if char in char_set:
+                char_index = char_set.find(char)
+                cipher_text += char_set[(char_index * key_a + key_b) % len(char_set)]
+            else:
+                cipher_text += char # leave this character unencrypted
+        print cipher_text
+
+    def decrypt():
+        pass
 
     encrypt_decrypt()
 
 if __name__ == '__main__':
-    if len(argv) == 0:
-        text = input("Enter a string to be encrypted/decrypted using the affine cipher\n> ")
+    if len(argv) == 1:
+        text = raw_input("Enter a string to be encrypted/decrypted using the affine cipher\n> ")
         affine_cipher(text)
     else:
         text = ''
