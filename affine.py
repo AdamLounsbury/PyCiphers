3bd7b8088ee3f1823fe8ae20ffbd2dab444f417a
Adam <alounsbu@alumni.uwo.ca>
2017-01-04 19:11:46 -0500

    Initial setup - main function calling

--- affine.py
+++ affine.py
@@ -4,3 +4,21 @@
 from crypto_funcs import *
 
 
+affine_cipher(txt):
+    pass
+
+
+
+if __name__ == '__main__':
+    text = ''
+    script = argv
+    word_num = len(argv)
+
+    # append command line string arguments into a variable
+    for k in range(1, word_num):
+        if k == (word_num - 1):
+            text += argv[k]
+        else:
+            text += argv[k] + ' '
+
+    affine_cipher(text)
