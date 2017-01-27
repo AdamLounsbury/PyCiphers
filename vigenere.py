5bb7c11d400ff15a24158a518b408cf54f1d1708
Adam <alounsbu@alumni.uwo.ca>
2017-01-27 18:06:25 -0500

    Code refactoring to reflect new modules cipher_funcs and main

--- vigenere.py
+++ vigenere.py
@@ -1,105 +1,65 @@
-
 import string
-from sys import argv
+import sys
+from cipher_funcs import CipherFuncs, cmd_handles
 
 char_set = string.ascii_lowercase
 
 
-def vigenere_cipher(message, user_option=None, user_key=None):
+def vigenere(text="", option="", key=""):
+    vigenere_cipher = CipherFuncs(text, option, key)
 
-    def encrypt_decrypt_prompt():
-        option = raw_input("Encrypt/Decrypt (e/d)?: ")
-        if check_option(option):
-            return key_get(option)
+    if not text and not option and not key:  # if vigenere is run as a script with no arguments
+        text, option, key = vigenere_cipher.script_call()
+        return encrypt_decrypt(text, option, key)
+
+    elif not text and not key:  # if vigenere is called from main.py, enc/dec is specified in the menu choice
+        text, key = vigenere_cipher.main_call()
+        return encrypt_decrypt(text, option, key)
+
+    elif text and not option and not key: # if vigenere is called directly from a CLI with an included string
+        option, key = vigenere_cipher.cli_call()
+        return encrypt_decrypt(text, option, key)
+
+    else:  # if vigenere is run in the shell with all arguments provided
+        valid_choices = vigenere_cipher.shell_call()
+        if valid_choices:
+            return encrypt_decrypt(text, option, key)
         else:
-            return encrypt_decrypt_prompt()
+            print "Try again!\n"
 
-    def key_get(option):
-        key = raw_input("Enter an encryption/decryption key: ")
-        key = check_key(key)
-        if key:
-            return encrypt_decrypt(option, key)
+
+def encrypt_decrypt(message, option, key):
+    cipher_text = ""
+    key_index = 0
+
+    for char in message:
+        if char.lower() in char_set:  # alphabet provided is all lower case - used to search for capital letters
+            msg_let_index = char_set.find(char.lower())  # ex: '20' for t
+            key_let_index = char_set.find(key[key_index])  # ex: '2' for b
+
+            if option.startswith('e'):
+                encode_text = (msg_let_index + key_let_index) % len(char_set)
+            elif option.startswith('d'):
+                encode_text = (msg_let_index - key_let_index) % len(char_set)
+
+            if char.islower():
+                cipher_text += char_set[encode_text]
+            elif char.isupper():
+                cipher_text += char_set[encode_text].upper()
+
+            key_index += 1
+            if key_index == len(key):  # when the end of the key is reached, wrap back to the start
+                key_index = 0
+
         else:
-            return key_get(option)
+            cipher_text += char
 
-    def check_option(option):
-        if option.startswith('E') or option.startswith('e'):
-            option = option.lower()
-            return option
-        elif option.startswith('D') or option.startswith('d'):
-            option = option.lower()
-            return option
-        else:
-            print 'Invalid operation detected. Please enter a valid operation (encryption/decryption).'
-            return False
-
-    def check_key(key):
-
-        try:
-            key = key.replace(" ", "")  # in case the user enters a string key containing spaces
-        except AttributeError:
-            print "Please enter a string key."
-            return False
-
-        if not isinstance(key, str):
-            print "Please enter a string key."
-            return False
-        elif not key.isalpha():
-            print "Please enter a string key containing only letters."
-            return False
-        else:
-            key = key.lower()  # in case the user input capital letters
-            return key
-
-    def encrypt_decrypt(option, key):
-        cipher_text = ""
-        key_index = 0
-
-        for char in message:
-            if char.lower() in char_set:  # alphabet provided is all lower case - used to search for capital letters
-                msg_let_index = char_set.find(char.lower())    # ex: '20' for t
-                key_let_index = char_set.find(key[key_index])  # ex: '2' for b
-
-                if option.startswith('e'):
-                    encode_text = (msg_let_index + key_let_index) % len(char_set)
-                elif option.startswith('d'):
-                    encode_text = (msg_let_index - key_let_index) % len(char_set)
-                
-                if char.islower():
-                    cipher_text += char_set[encode_text]
-                elif char.isupper():
-                    cipher_text += char_set[encode_text].upper()
-
-                key_index += 1
-                if key_index == len(key):  # when the end of the key is reached, wrap back to the start
-                    key_index = 0
-
-            else:
-                cipher_text += char
-
-        print cipher_text
-
-    if (user_option is not None) and (user_key is not None):
-        if check_option(user_option) and check_key(user_key):
-            encrypt_decrypt(user_option, user_key)
-    else:
-        return encrypt_decrypt_prompt()
-
+    print cipher_text
 
 if __name__ == "__main__":
-    if len(argv) == 1:
-        text = raw_input("Enter a string to be encrypted/decrypted using the vigenere cipher\n> ")
-        vigenere_cipher(text)
-    else:  # handle input from command line
-        text = ""
-        script = argv
-        word_num = len(argv)
+    try:
+        msg = cmd_handles(sys.argv)
+    except NameError:
+        raise NameError("This module should be in the same folder as cipher_funcs.py")
 
-        # append command line string arguments into a variable
-        for k in range(1, word_num):
-            if k == (word_num - 1):
-                text += argv[k]
-            else:
-                text += argv[k] + " "
-
-        vigenere_cipher(text)
+    vigenere(msg)
