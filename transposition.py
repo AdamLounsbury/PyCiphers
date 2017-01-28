a07ba5d3adb3b07f8dd49d3e4494149012252cf5
Adam <alounsbu@alumni.uwo.ca>
2017-01-28 14:25:04 -0500

    Completely re-factored to use cipher_funcs with main.py functionality as well

--- transposition.py
+++ transposition.py
@@ -1,89 +1,62 @@
 #!/usr/bin/env python
 # ACL 2016 - alounsbu@alumni.uwo.ca
 
-from math import ceil
-from sys import argv
+import math
+import sys
+from cipher_funcs import CipherFuncs, cmd_handles
 
 
-def transposition_cipher(txt, user_option=None, user_key=None):
-    """Encode or decode a string using the transposition cipher."""
+def transposition(text="", option="", key=""):
+    transposition_cipher = CipherFuncs(text, option, key)
+    text, option, key = transposition_cipher.call_source()
 
-    def encrypt_decrypt():
-        """Retrieve cipher operation(encrypt/decrypt) and cipher key (if not using 'all') information from the user"""
-        while True:
-            option = raw_input("Encrypt/Decrypt (e/d)?: ")
-            if option == 'd' or option == 'e':
-                break
-            else:
-                print 'Please choose a valid option for decryption/encryption.'
+    return encrypt_decrypt(text, option, int(key))  # cast str key to int
 
-        while True:
-            try:
-                transpos_key = int(raw_input('Enter an encryption/decryption key: '))  # indicates 'row' size
-                if int(transpos_key):
-                    operation(option, transpos_key)
-                    break
-            except ValueError:
-                print "Please enter an integer value for the encryption key."
 
-    def operation(enc_dec, transpos_key):
+def encrypt_decrypt(message, option, key):
+    str_len = len(message)  # length of the input text
+    col_size = int(math.ceil(float(str_len) / key))  # encoding column size
 
-        str_len = len(txt) # length of the input text
-        col_size = int(ceil(float(str_len) / transpos_key)) # encoding column size
-
-        def encode():
-            encode_new_str = [''] * transpos_key
-
-            for i in range(transpos_key):
-                col_loc = 0  # current location in a column
-                for j in range(col_size):
-                    if (i + col_loc) < str_len:
-                        encode_new_str[i] += txt[i + col_loc]
-                        col_loc += transpos_key
-
-            print ''.join(encode_new_str)
-
-        def decode():
-            no_char = (col_size * transpos_key) - str_len  # number of 'shaded' boxes
-            decode_new_str = [''] * col_size
-
-            row = 0
-            col = 0
-
-            for char in txt:
-                decode_new_str[col] += char
-                col += 1
-
-                if (col == col_size) or (col == col_size - 1 and row >= transpos_key - no_char):
-                    col = 0
-                    row += 1
-
-            print ''.join(decode_new_str)
-
-        if ('e' or 'E') in enc_dec:
-            return encode()
-        else:
-            return decode()
-
-    if (user_option is not None) and (user_key is not None):
-        return operation(user_option, user_key)
+    if option.startswith('e'):
+        return encrypt(message, col_size, key, str_len)
     else:
-        encrypt_decrypt()
+        return decrypt(message, col_size, key, str_len)
 
-if __name__ == '__main__':
-    if len(argv) == 1:
-        text = raw_input("Enter a string to be encrypted/decrypted using the caesar cipher\n> ")
-        transposition_cipher(text)
-    else:  # handle input from command line
-        text = ""
-        script = argv
-        word_num = len(argv)
 
-        # append command line string arguments into a variable
-        for k in range(1, word_num):
-            if k == (word_num - 1):
-                text += argv[k]
-            else:
-                text += argv[k] + " "
+def encrypt(message, col_size, key, str_len):
+    encode_new_str = [''] * key
 
-        transposition_cipher(text)
+    for i in range(key):
+        col_loc = 0  # current location in a column
+        for j in range(col_size):
+            if (i + col_loc) < str_len:
+                encode_new_str[i] += message[i + col_loc]
+                col_loc += key
+
+    print ''.join(encode_new_str)
+
+
+def decrypt(message, col_size, key, str_len):
+    no_char = (col_size * key) - str_len  # number of 'shaded' boxes
+    decode_new_str = [''] * col_size
+
+    row = 0
+    col = 0
+
+    for char in message:
+        decode_new_str[col] += char
+        col += 1
+
+        if (col == col_size) or (col == col_size - 1 and row >= key - no_char):
+            col = 0
+            row += 1
+
+    print ''.join(decode_new_str)
+
+if __name__ == "__main__":
+    try:
+        msg = cmd_handles(sys.argv)
+    except NameError:
+        raise NameError("This module should be in the same folder as cipher_funcs.py")
+
+    transposition(msg)
