551d43eceaccce975d0dc0fa9ffb9147cf670af6
Adam <alounsbu@alumni.uwo.ca>
2017-01-29 18:30:46 -0500

    Clipboard functionality moved to main.py

--- vigenere.py
+++ vigenere.py
@@ -13,8 +13,7 @@ def vigenere(text="", option="", key=""):
     text, option, key = vigenere_cipher.call_source()
 
     cipher_text = encrypt_decrypt(text, option, key)
-    print cipher_text
-    clipboard(cipher_text)
+    return cipher_text
 
 
 def encrypt_decrypt(message, option, key):
